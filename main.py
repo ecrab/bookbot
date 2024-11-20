ee52c73c43aefc3a35af498d19883cb70217f84a
ecrab <elliscrabtree@gmail.com>
2024-11-20 14:12:06 -0600

    final changes to main

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = count_chars(text)
+    chars_sorted = chars_dict_to_sorted(chars_dict)
 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the book")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,5 +23,27 @@ def count_words(text):
     words = text.split()
     count = len(words)
     return count
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def count_chars(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+    
 
 main()
